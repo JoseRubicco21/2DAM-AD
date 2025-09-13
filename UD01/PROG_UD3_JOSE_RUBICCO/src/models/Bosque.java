8066d473f21171d743ab945deb26c585aa66d4b5
JoseRubicco21 <a23joserf@iessanclemente.net>
2025-09-13 17:39:36 +0200

    feat: :sparkles: Add the base classes implementation.
    Added all the base classes implementations.

--- UD01/PROG_UD3_JOSE_RUBICCO/src/models/Bosque.java
+++ UD01/PROG_UD3_JOSE_RUBICCO/src/models/Bosque.java
@@ -1,5 +1,43 @@
 package models;
 
+import models.monster.Monstruo;
+
 public class Bosque {
+    private String nombre;
+    private int nivelPeligro;
+    private Monstruo monstruoJefe;
+    
+    public void mostarJefe(){
+        System.out.println(this.monstruoJefe);
+    }
+
+    public void cambiarJefe(Monstruo newBoss){
+        this.setMonstruoJefe(newBoss);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNivelPeligro() {
+        return nivelPeligro;
+    }
+
+    public void setNivelPeligro(int nivelPeligro) {
+        this.nivelPeligro = nivelPeligro;
+    }
+
+    public Monstruo getMonstruoJefe() {
+        return monstruoJefe;
+    }
+
+    public void setMonstruoJefe(Monstruo monstruoJefe) {
+        this.monstruoJefe = monstruoJefe;
+    }
+
     
 }
