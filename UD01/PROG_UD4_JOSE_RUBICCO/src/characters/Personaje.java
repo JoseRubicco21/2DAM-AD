a9bc80faac02d3af02ef2f3bfb5354f5c9874c53
JoseRubicco21 <a23joserf@iessanclemente.net>
2025-09-14 13:02:11 +0200

    feat: :sparkles: Implement the personaje class.
    Implemented the Personaje class to a base level.

--- UD01/PROG_UD4_JOSE_RUBICCO/src/characters/Personaje.java
+++ UD01/PROG_UD4_JOSE_RUBICCO/src/characters/Personaje.java
@@ -2,4 +2,36 @@ package characters;
 
 public abstract class Personaje {
     
+    String nombre;
+    int vida;
+
+    public Personaje(String nombre, int vida) {
+        this.nombre = nombre;
+        this.vida = vida;
+    }
+
+    public Personaje() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = (vida < 0) ? 0 : vida;
+    }
+
+    public abstract void atacar();
+
+    public void takeDamage(int damage){
+        this.setVida(this.getVida() - damage);
+    }
 }
