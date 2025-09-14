50825dc20bc7edda55b5ea3ae3b7c3a617ad1f6a
JoseRubicco21 <a23joserf@iessanclemente.net>
2025-09-14 15:51:06 +0200

    feat: :construction: Create Initialization for objects in main.
    Created the initialization of monster and mage objects.

--- UD01/PROG_UD4_JOSE_RUBICCO/src/App.java
+++ UD01/PROG_UD4_JOSE_RUBICCO/src/App.java
@@ -1,5 +1,32 @@
+import characters.mage.Hechizo;
+import characters.mage.Mago;
+import characters.monster.Monstruo;
+import characters.monster.TipoDeMonstruo;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Mago> magos = new ArrayList<>();
+        ArrayList<Monstruo> monstruos = new ArrayList<>();    
+        
+        // Crear algunos magos de ejemplo
+        Mago mago1 = new Mago("Gandalf", 100, 3, Hechizo.PIROMANCIA);
+        Mago mago2 = new Mago("Merlin", 80, 4, Hechizo.INVOCACION);
+        Mago mago3 = new Mago().createMago(sc);
+        magos.add(mago1);
+        magos.add(mago2);
+        magos.add(mago3);
+
+        // Crear un monstruo de ejemplo
+        Monstruo monstruo1 = new Monstruo("Bob el temible", 30, TipoDeMonstruo.OGRO, 2);
+        Monstruo monstruo2 = new Monstruo("Kevin, Destructor de mundos", 50, TipoDeMonstruo.TROLL, 3);
+        Monstruo monstruo3 = new Monstruo().creaMonstruo(sc);
+        monstruos.add(monstruo1);
+        monstruos.add(monstruo2);
+        monstruos.add(monstruo3);
+    
     }
 }
