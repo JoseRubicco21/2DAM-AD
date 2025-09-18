0f971281130f7811a35f47f44eb23ab361c4add3
JoseRubicco21 <a23joserf@iessanclemente.net>
2025-09-18 10:20:22 +0200

    feat: :lock: Implement Exceptions
    Implemented basic exception handling

--- UD01/AD_UD1-AT.02-File/src/App.java
+++ UD01/AD_UD1-AT.02-File/src/App.java
@@ -1,24 +1,44 @@
 import java.io.FilenameFilter;
+import java.io.IOException;
 
 import ejercicio_1.Filtrar;
 import ejercicio_1.FiltrarNombre;
 import java.io.File;
+import java.io.FileNotFoundException;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         
 
-        // Part 1 of the excercise.
-        Filtrar filtrar = new Filtrar();
-        System.out.println("filtrando con filtrar.fitlrar");
-        filtrar.filtrar("./src/ejercicio_1", "java");
+        try {
+            // Part 1 of the excercise.
+            Filtrar filtrar = new Filtrar();
+            System.out.println("filtrando con filtrar.fitlrar");
+            filtrar.filtrar("./src/ejercicio_1", "java");
+            
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
 
-        // Part 2 of the excercise.
-        File dir = new File("./src/ejercicio_1");
-        String[] ficherosFiltrados = dir.list(new FiltrarNombre("Filtrar"));
-        System.out.println("Filtrando con el filenameFilter");
-        for(int i = 0; i < ficherosFiltrados.length; i++){
-            System.out.println(ficherosFiltrados[i]);
+        try {
+            // Part 2 of the excercise.
+            File dir = new File("./src/ejercicio_1");
+            if(!dir.exists()) throw new FileNotFoundException("El archivo no existe.");
+            if(dir.isFile()) throw new IOException("El archivo no es un directorio.");
+            String[] ficherosFiltrados = dir.list(new FiltrarNombre("Filtrar"));
+            System.out.println("Filtrando con el filenameFilter");
+            for(int i = 0; i < ficherosFiltrados.length; i++){
+                System.out.println(ficherosFiltrados[i]);
+            } 
+        }
+        catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+        catch (IOException e){
+            System.out.println(e.getMessage());
         }
     }
 }
+
--- UD01/AD_UD1-AT.02-File/src/ejercicio_1/Filtrar.java
+++ UD01/AD_UD1-AT.02-File/src/ejercicio_1/Filtrar.java
@@ -4,21 +4,22 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.function.Predicate;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Filtrar {
 
-    public void filtrar(String ruta, String extension){
+    public void filtrar(String ruta, String extension) throws IOException, FileNotFoundException{
         // Create the route
         File dir = new File(ruta);
-
+        if(!dir.exists()) throw new FileNotFoundException("Fichero no encontrado");
+        if(dir.isFile()) throw new IOException("El fichero es un archivo, no un directorio");
         // get all the named files in the directory
         ArrayList<File> files = new ArrayList<>(List.of(dir.listFiles()));
-        
         // Create a predicate to use as a function for filtering.
         Predicate<File> DoesNotEndWithExtension = file -> !file.getName().endsWith(extension);
         // Remove if matches with predicate.
         files.removeIf(DoesNotEndWithExtension);
-
         // Print every name in the files collection with a method reference.
         files.forEach(System.out::println);
     }
