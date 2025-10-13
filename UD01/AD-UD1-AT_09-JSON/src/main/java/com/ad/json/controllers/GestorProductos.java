13499f5a3fffeffaea777c7934151ed97eb54208
JoseRubicco21 <a23joserf@iessanclemente.net>
2025-10-13 09:44:45 +0200

    feat: :sparkles: Create the save to Json in the controller of Productos.
    Created the method saveToJson in the controller.

--- UD01/AD-UD1-AT_09-JSON/src/main/java/com/ad/json/controllers/GestorProductos.java
+++ UD01/AD-UD1-AT_09-JSON/src/main/java/com/ad/json/controllers/GestorProductos.java
@@ -1,5 +1,39 @@
 package com.ad.json.controllers;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import com.ad.json.models.Producto;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class GestorProductos {
     
+
+    public static void saveToJson(ArrayList<Producto> productos){
+        try(FileWriter fw = new FileWriter(manageCreationOfJsonFile())){
+            GsonBuilder gBuilder = new GsonBuilder().setPrettyPrinting();
+            Gson gson = gBuilder.create();
+            gson.toJson(productos);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // Manage the creation of the productos.json file in the resources folder.
+    private static File manageCreationOfJsonFile() throws IOException{
+        Path jsonFilePath = Paths.get("resources", "productos.json");
+        File jsonFile = jsonFilePath.toFile();
+
+        if(!jsonFile.exists()){
+            jsonFile.mkdirs();
+            jsonFile.createNewFile();
+        }
+
+        return jsonFile;
+    }
 }
